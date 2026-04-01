ad3a09dccc1b5f9b90d58838ddb74e37400a7d37
Lance Thongsavanh <lancer8106@gmail.com>
2026-03-31 21:29:08 -0400

    Imported pytest. Imported the city_country file from the city_functions file. Created the test case with the parameters 'santiago' and 'chile' as the arguments.

---
+++ test_cities.py
@@ -0,0 +1,8 @@
+"""
+Write a function called test_city_country() to verify that calling your function with values such as 'santiago' and 'chile' results in the correct string
+"""
+import pytest
+from functions.city_functions import city_country
+
+def test_city_country() -> None:
+    assert city_country("santiago", "chile") == "Santiago, Chile"
