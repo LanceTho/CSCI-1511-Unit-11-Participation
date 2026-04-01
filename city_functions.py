f83f7c5bf08a26db601351f4ed30ed54d229403d
Lance Thongsavanh <lancer8106@gmail.com>
2026-03-31 21:20:35 -0400

    Created the city_country function that has 2 parameters for the city name and country name.

--- city_functions.py
+++
@@ -1,3 +0,0 @@
-"""
-Write a function that accepts two parameters: a city name and a country name. The function should return a single string of the form City, Country, such as Santiago, Chile.
-"""
